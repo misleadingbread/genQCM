bc9ba1771146a644d6741b6343dda35d7610e510
misleadingbread <quentinkomouth@gmail.com>
2025-03-03 11:51:04 +0100

    creation classe genquestionqcm

---
+++ main.py
@@ -0,0 +1,6 @@
+class Genquestionqcm:
+    def __init__(question,reponses,juste):
+        self.question = question
+        self.reponses = reponses #cest genre une liste
+        self.juste = juste
+
