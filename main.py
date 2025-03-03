ebce5c7fc8cc68909fef7677187d77c45fb730e7
natwltz <nathan.waltzer@stagiairesmns.fr>
2025-03-03 13:32:29 +0100

    Ajout de la fonction lectureFichier qui ajoute les questions d'un texte dans une liste en séléctionnant les lignes qui finissent par un "?"

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import os
+
 class Genquestionqcm:
     def __init__(self, question,reponses,juste):
         self.question = question
-        self.reponses = reponses #cest genre une liste
+        self.reponses = reponses #liste des réponses
         self.juste = juste
+
+def lectureFichier(nomDeFichier):
+    """Permet de récupérer les questions d'un fichier .txt"""
+    listeQuestions = []
+    with open(nomDeFichier, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            if line.endswith("?"):
+                listeQuestions.append(line)
+    return listeQuestions
+            
+print(lectureFichier(r"C:\Users\natha\OneDrive\Bureau\Cours\Algo\genQCM\QCM_cinema.txt"))
 
