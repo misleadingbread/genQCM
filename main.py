33b87673ecdd42ac965e7fc480b80238b9da042f
natwltz <nathan.waltzer@stagiairesmns.fr>
2025-03-04 13:37:05 +0100

    Ajout de la récupérations des propositions et des réponses aux questions

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ class Genquestionqcm:
 def lectureFichier(nomDeFichier):
     """Permet de récupérer les questions d'un fichier .txt"""
     listeQuestions = []
+    index = 0
     with open(nomDeFichier, 'r', encoding='utf-8') as file:
         for line in file:
             line = line.strip()
+            #Récupère les questions grâce au ligne finissant par un "?"
             if line.endswith("?"):
-                listeQuestions.append(line)
+                question = line
+                r1 = line.readline()
+                r2 = line.readline()
+                r3 = line.readline()
+                r4 = line.readline()
+                reponses = [r1,r2,r3,r4]
+                rep = line.readline()
+                listeQuestions.append(Genquestionqcm(question, reponses, rep))
+
     return listeQuestions
             
 print(lectureFichier(r"C:\Users\natha\OneDrive\Bureau\Cours\Algo\genQCM\QCM_cinema.txt"))
