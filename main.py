598922c78f7d8a1bdd1c6abb7c7481451b1a26d5
misleadingbread <quentinkomouth@gmail.com>
2025-03-10 11:19:01 +0100

    ajout de la representation de la classe genquestionqcm, modification delecturefichier

--- main.py
+++ main.py
@@ -6,25 +6,28 @@ class Genquestionqcm:
         self.reponses = reponses #liste des réponses
         self.juste = juste
 
+    def __str__(self):
+        return f"question: {self.question}\nreponses: {', '.join(self.reponses)}\nreponse correcte: {self.juste}\n"
+
 def lectureFichier(nomDeFichier):
     """Permet de récupérer les questions d'un fichier .txt"""
     listeQuestions = []
-    index = 0
     with open(nomDeFichier, 'r', encoding='utf-8') as file:
-        for line in file:
-            line = line.strip()
-            #Récupère les questions grâce au ligne finissant par un "?"
+        lines = file.readlines()
+        index = 0
+        while index < len(lines):
+            line = lines[index].strip()
             if line.endswith("?"):
                 question = line
-                r1 = line.readline()
-                r2 = line.readline()
-                r3 = line.readline()
-                r4 = line.readline()
-                reponses = [r1,r2,r3,r4]
-                rep = line.readline()
-                listeQuestions.append(Genquestionqcm(question, reponses, rep))
-
-    return listeQuestions
+                rep_juste = lines[index+5].strip()
+                reponses = [lines[index+1].strip(),lines[index+2].strip(),lines[index+3].strip(),lines[index+4].strip()]
+                listeQuestions.append(Genquestionqcm(question, reponses, rep_juste))
+                index += 6
+            else:
+                index += 1
+        return listeQuestions
             
-print(lectureFichier(r"C:\Users\natha\OneDrive\Bureau\Cours\Algo\genQCM\QCM_cinema.txt"))
-
+#print(lectureFichier(r"C:\Users\natha\OneDrive\Bureau\Cours\Algo\genQCM\QCM_cinema.txt"))
+questions = lectureFichier(r"C:\Users\quentin\Documents\coursbsd2024\genQCM\QCM_culture_generale.txt")
+for question in questions:
+    print(question)
